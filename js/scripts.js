26a24148d54479aafe4539e71547fbda4d6f0582
its-gio <me@its-g.io>
2018-10-30 20:56:16 -0700

    Showing 3 items now just need to fix when looping through.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 let productSlide = document.querySelectorAll(".product");
-let sliderLength = productSlide.length;
 let arrowRight = document.querySelector("#pp-right-arrow");
 let arrowLeft = document.querySelector("#pp-left-arrow");
-let slides = 0;
+let sliderLength = productSlide.length;
+let item1 = 0;
+let item2 = 1;
+let item3 = 2;
 
 // Clear all images
 function reset() {
@@ -19,33 +21,52 @@ function changeDisplay(item) {
 function startCarousel() {
   reset();
   changeDisplay(productSlide[0]);
+  changeDisplay(productSlide[1]);
+  changeDisplay(productSlide[2]);
 }
 
 // Show Next
 function slideRight() {
   reset();
-  changeDisplay(productSlide[slides + 1]);
-  slides++;
+  item1++;
+  item2++;
+  item3++;
+  changeDisplay(productSlide[item1]);
+  changeDisplay(productSlide[item2]);
+  changeDisplay(productSlide[item3]);
 }
 
 // Show Prev
 function slideLeft() {
   reset();
-  changeDisplay(productSlide[slides - 1]);
-  slides--;
+  item1--;
+  item2--;
+  item3--;
+  console.log(item1);
+  console.log(item2);
+  console.log(item3);
+  changeDisplay(productSlide[item3]);
+  changeDisplay(productSlide[item2]);
+  changeDisplay(productSlide[item1]);
 }
 
 // Adding Event Listeners
 arrowLeft.addEventListener("click", () => {
-  if (slides == 0) {
-    slides = sliderLength;
+  if (item1 == 0) {
+    item1 = sliderLength;
+  }
+  if (item2 == 0) {
+    item2 = sliderLength;
+  }
+  if (item3 == 0) {
+    item3 = sliderLength;
   }
   slideLeft();
 });
 
 arrowRight.addEventListener("click", () => {
-  if (slides == sliderLength - 1) {
-    slides = -1;
+  if (item1 == sliderLength - 1) {
+    item1 = -1;
   }
   slideRight();
 });
