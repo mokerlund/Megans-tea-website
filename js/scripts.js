aa3b3242d69ba7f3f41fd805a84c422649eb8968
its-gio <me@its-g.io>
2018-10-30 22:07:16 -0700

    Switching the code so it adds and removes a class 'active'. Still tweaking the loop problem

--- js/scripts.js
+++ js/scripts.js
@@ -7,58 +7,50 @@ let item2 = 1;
 let item3 = 2;
 
 // Clear all images
-function reset() {
-  for (let i = 0; i < sliderLength; i++) {
-    productSlide[i].style.display = "none";
+function reset(dir) {
+  if (dir == "right") {
+    productSlide[item1].classList.remove("active");
+  } else {
+    productSlide[item3].classList.remove("active");
   }
 }
 
 function changeDisplay(item) {
-  return (item.style.display = "inline-block");
+  item[item1].classList.add("active");
+  item[item2].classList.add("active");
+  item[item3].classList.add("active");
 }
 
 // Init Slider (Showing 3)
 function startCarousel() {
-  reset();
-  changeDisplay(productSlide[0]);
-  changeDisplay(productSlide[1]);
-  changeDisplay(productSlide[2]);
+  changeDisplay(productSlide);
 }
 
 // Show Next
 function slideRight() {
-  reset();
+  reset("right");
   item1++;
   item2++;
   item3++;
-  changeDisplay(productSlide[item1]);
-  changeDisplay(productSlide[item2]);
-  changeDisplay(productSlide[item3]);
+  changeDisplay(productSlide);
 }
 
 // Show Prev
 function slideLeft() {
-  reset();
+  reset("left");
   item1--;
   item2--;
   item3--;
-  console.log(item1);
-  console.log(item2);
-  console.log(item3);
-  changeDisplay(productSlide[item3]);
-  changeDisplay(productSlide[item2]);
-  changeDisplay(productSlide[item1]);
+  changeDisplay(productSlide);
 }
 
 // Adding Event Listeners
 arrowLeft.addEventListener("click", () => {
   if (item1 == 0) {
     item1 = sliderLength;
-  }
-  if (item2 == 0) {
+  } else if (item2 == 0) {
     item2 = sliderLength;
-  }
-  if (item3 == 0) {
+  } else if (item3 == 0) {
     item3 = sliderLength;
   }
   slideLeft();
@@ -67,6 +59,10 @@ arrowLeft.addEventListener("click", () => {
 arrowRight.addEventListener("click", () => {
   if (item1 == sliderLength - 1) {
     item1 = -1;
+  } else if (item2 == sliderLength - 1) {
+    item2 = -1;
+  } else if (item3 == sliderLength - 1) {
+    item3 = -1;
   }
   slideRight();
 });
